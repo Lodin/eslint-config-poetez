96ad09adf45fa408c1cff91dd53ee1305743b145
Vlad Rindevich <rindevich.vs@gmail.com>
2020-07-29 00:24:40 +0300

    fix: allow using JSX in .tsx files

--- rules/typescript-react.js
+++ rules/typescript-react.js
@@ -1,4 +1,10 @@
 module.exports = {
+  rules: {
+    'react/jsx-filename-extension': [
+      'error',
+      {extensions: ['.js', '.jsx', '.tsx']},
+    ],
+  },
   settings: {
     'import/resolver': {
       node: {
