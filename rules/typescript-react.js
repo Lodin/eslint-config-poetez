cbceb4c426e8e61bb0e147ceb8d0ce9aa9143ec6
Vlad Rindevich <rindevich.vs@gmail.com>
2020-07-31 00:11:20 +0300

    feat(typescript): improve naming-convention rule

--- rules/typescript-react.js
+++ rules/typescript-react.js
@@ -13,7 +13,7 @@ module.exports = {
       },
       {
         selector: 'parameter',
-        format: ['camelCase'],
+        format: ['camelCase', 'PascalCase'],
         leadingUnderscore: 'allow',
       },
       {
